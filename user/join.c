4adbd0c075c6abb6df66908554868329956c98c3
DEMETRIUS CHATTERJEE <dchatterjee3@dons.usfca.edu>
2025-09-23 10:50:54 -0700

    Feature: Adding initial intergration of join as a system call

---
+++ user/join.c
@@ -0,0 +1,32 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    // ensure correct usage of command line args
+    if (argc != 3){
+        printf("Usage: join file1.txt file2.txt\n");
+        exit(1);
+    }
+
+    printf("Joining files '%s' and '%s'...\n", argv[1], argv[2]);
+
+
+    // initiate data structure for file content
+    // set two arrays of 50 lines and 256 chars each
+    char file1_lines[50][256];
+    char file2_lines[50][256];
+
+
+    // read both files
+    printf("Reading first file...");
+    int count1 = read_file_lines(argv[1], file1_lines, 50);
+
+    printf("Reading second file...");
+    int count2 = read_file_lines(argv[2], file2_lines, 50);
+    
+    // join both files
+    printf("joining both files");
+    join_files(file1_lines, count1, file2_lines, count2);
+
+    exit(0);
+}
